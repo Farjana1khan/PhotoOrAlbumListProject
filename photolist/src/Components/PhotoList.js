8aa4c7d01b84190549297347827c90e28cd2988a
Farjana <farjana09khan@gmail.com>
2022-01-29 14:09:43 +0530

    changes in photolist

--- photolist/src/Components/PhotoList.js
+++ photolist/src/Components/PhotoList.js
@@ -36,43 +36,40 @@ filterPhotos = () =>{
 
       render() {
             return (
-                 <section>
-                       <div className='row1'>
-                             <input type="text"
-                             onChange={this.filterPhotos}
-                             ref={node => (this.filterText = node)}
-                             className='searchtext'
-                              />
-                              <button onClick={this.filterphotos} 
-                              className='filterbutton' >Filter</button>
-                       </div>
-                       <hr/>
-
-                  <div className='row2'>
-                        {this.state.visiblePhotos.map(photo => {
-                              return (
-                                <div className="col">
-                                  <div className="card">
-                                    <div className="card-image">
-                                      <img src={photo.thumbnailUrl} alt='photos' />
-                                      <span className="card-title">
-                                        {photo.title}
-                                      </span>
-                                    </div>
-                                    <div className="card-action">
-                                      <NavLink to={`${photo.id}`} className="navlink" >
-                                        This is a link
-                                      </NavLink>
-                                    </div>
-                                  </div>
-                                </div>
-                              );
-                        })}
-                       
-
-                  </div>
-
-                 </section>
-            )
+              <section>
+                <div className="row1">
+                  <input
+                    type="text"
+                    onChange={this.filterPhotos}
+                    ref={(node) => (this.filterText = node)}
+                    className="searchtext"
+                  />
+                  <button onClick={this.filterphotos} className="filterbutton">
+                    Filter
+                  </button>
+                </div>
+                <hr />
+                <span className='list'>PhotoList</span>
+                <div className="row2">
+                  {this.state.visiblePhotos.map((photo) => {
+                    return (
+                      <div className="col">
+                        <div className="card">
+                          <div className="card-image">
+                            <img src={photo.thumbnailUrl} alt="photos" />
+                            <span className="card-title">{photo.title}</span>
+                          </div>
+                          <div className="card-action">
+                            <NavLink to={`${photo.id}`} className="navlink">
+                              This is a link
+                            </NavLink>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </section>
+            );
       }
 }
